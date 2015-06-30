58e751b43f155819f956f6ecfb1ee4aae974b9dd
Andrew Henderson <hendersa@icculus.org>
2015-06-30 01:26:40 +0000

    Added .dtb files for the LCD3/4/7 capes with and without audio rev B cape

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 echo Building BES device tree files...
 make clean
+# Built-in HDMI audio and video
 make src/arm/bes-hdmi.dtb
+# lcd3-01-00a2
 make src/arm/bes-lcd3.dtb
+# lcd3-01-00a2 and audio revb
+make src/arm/bes-lcd3-audio.dtb
+# lcd4-01-00a1
 make src/arm/bes-lcd4.dtb
+# lcd4-01-00a1 and audio revb
+make src/arm/bes-lcd4-audio.dtb
+# lcd7-01-00a3
 make src/arm/bes-lcd7.dtb
-make src/arm/bes-4dcape43t.dtb
-make src/arm/bes-4dcape70t.dtb
+# lcd7-01-00a3 and audio revb
+make src/arm/bes-lcd7-audio.dtb
 
 echo Copying device tree files into \"deploy\" directory...
 rm -f deploy/*
